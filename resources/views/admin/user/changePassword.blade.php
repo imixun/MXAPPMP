9dc3325e9fc38bf639a33f221574c454691b0801
moesk <279737778@qq.com>
2016-04-25 14:07:39 +0800

    添加修改密码模板

---
+++ resources/views/admin/user/changePassword.blade.php
@@ -0,0 +1,68 @@
+@extends('admin.layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading">修改密码</div>
+                <div class="panel-body">
+                    <form class="form-horizontal" role="form" method="POST">
+                        {!! csrf_field() !!}
+
+                        <div class="form-group{{ $errors->has('old_password') ? ' has-error' : '' }}">
+                            <label class="col-md-4 control-label">原密码</label>
+
+                            <div class="col-md-6">
+                                <input type="password" class="form-control" name="old_password" value="{{ old('name') }}" >
+
+                                @if ($errors->has('old_password'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('old_password') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('new_password') ? ' has-error' : '' }}">
+                            <label class="col-md-4 control-label">新密码</label>
+
+                            <div class="col-md-6">
+                                <input type="password" class="form-control" name="new_password" >
+
+                                @if ($errors->has('new_password'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('new_password') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('new_password_confirmation') ? ' has-error' : '' }}">
+                            <label class="col-md-4 control-label">确认密码</label>
+
+                            <div class="col-md-6">
+                                <input type="password" class="form-control" name="new_password_confirmation" >
+
+                                @if ($errors->has('new_password_confirmation'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('new_password_confirmation') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    修改
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
